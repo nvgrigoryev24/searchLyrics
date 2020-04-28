d8938c34993795d2f17ca49227088e065a878dfa
Nikolay Grigoryev <nvgrigoryev24@gmail.com>
2020-04-29 00:21:08 +0700

    finished proj

---
+++ app.js
@@ -0,0 +1,82 @@
+const form = document.getElementById('form'),
+  search = document.getElementById('search'),
+  result = document.getElementById('result'),
+  more = document.getElementById('more');
+
+
+
+const apiURL = 'https://api.lyrics.ovh';
+
+/* FUNCTIONS */
+
+async function searchSongs(term) {
+  const res = await fetch(`${apiURL}/suggest/${term}`);
+  const data = await res.json();
+
+  showData(data);
+}
+
+function showData(data) {
+  result.innerHTML = `
+  <ul class="songs">
+  ${data.data.map(song => ` <li>
+  <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+  <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Текст</button>
+  </li>`)
+      .join('')
+    }
+  </ul>
+  `;
+
+  if (data.prev || data.next) {
+    more.innerHTML =
+      `${data.prev ? `<button onclick="getMoreSongs('${data.prev}')" class="btn" >Назад</button>` : ''}
+  ${ data.next ? `<button onclick="getMoreSongs('${data.next}')" class="btn" >Вперед</button>` : ''}
+  `;
+  } else {
+    more.innerHTML = '';
+  }
+}
+
+
+async function getMoreSongs(url) {
+  const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+  const data = await res.json();
+  showData(data);
+}
+
+async function getLyrics(artist, songTitle) {
+  const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+  const data = await res.json();
+
+  const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+  result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+  <span>${lyrics}</span>`;
+
+  more.innerHTML = '';
+}
+
+/* EVENT LISTENERS */
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const searchTerm = search.value.trim();
+
+  if (!searchTerm) {
+    alert("Введите условия поиска");
+  } else {
+    searchSongs(searchTerm);
+  }
+});
+
+result.addEventListener('click', e => {
+  const clickedEl = e.target;
+  if (clickedEl.tagName === 'BUTTON') {
+    const artist = clickedEl.getAttribute('data-artist');
+    const songTitle = clickedEl.getAttribute('data-songtitle');
+
+    getLyrics(artist, songTitle);
+  }
+});
